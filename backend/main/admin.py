52b2d5a20fb6e255955d83673231421bbf9f9501
Gudzenko <o.gudzenko@weegree.com>
2024-06-29 14:30:10 +0200

    add models

--- backend/main/admin.py
+++ backend/main/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Device, Robot, Car, Guardian, Event
+
+admin.site.register(Device)
+admin.site.register(Robot)
+admin.site.register(Car)
+admin.site.register(Guardian)
+admin.site.register(Event)
--- backend/main/models.py
+++ backend/main/models.py
@@ -1,3 +1,44 @@
 from django.db import models
 
-# Create your models here.
+
+class Device(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Robot(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Car(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Guardian(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Event(models.Model):
+    name = models.CharField(max_length=255)
+    start_datetime = models.DateTimeField()
+    end_datetime = models.DateTimeField()
+    city = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    robots = models.ManyToManyField(Robot, blank=True)
+    devices = models.ManyToManyField(Device, blank=True)
+    cars = models.ManyToManyField(Car, blank=True)
+    guardians = models.ManyToManyField(Guardian, blank=True)
+
+    def __str__(self):
+        return f"{self.name}/{self.city}/{self.start_datetime}"
